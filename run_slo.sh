423f72662dfba9c8c69707bba0342da7f8708bfd
teddy <teddytw1209@gmail.com>
2025-08-13 21:21:39 -0400

    script

---
+++ run_slo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=slo_extract
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=NONE
+#SBATCH --ntasks=1                 # Number of MPI tasks (i.e. processes)
+#SBATCH --cpus-per-task=10           # Number of cores per MPI task 
+#SBATCH --nodes=1                    # Maximum number of nodes to be allocated
+#SBATCH --mem=20g
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.%j.out
+
+pwd; hostname; date
+
+module load conda
+conda activate oct-analysis
+
+DATA_CSV=$1
+
+python3 usage_slo.py --analysis_csv $DATA_CSV
---
+++ usage_slo.py
@@ -0,0 +1,17 @@
+import argparse
+from sloctolyzer import main_dicom
+
+def main():
+    parser = argparse.ArgumentParser(description="Run main_dicom with custom parameters.")
+
+    parser.add_argument("--analysis_csv", default=r"/blue/ruogu.fang/tienyuchang/OCT_EDA/Paired_OCT_Data_ADCON_samples_part2.csv", help="Path to the analysis CSV file")
+    parser.add_argument("--output_directory", default=r"/blue/ruogu.fang/tienyuchang/SLO_Output", help="Directory to save the output")
+    parser.add_argument("--robust_run", type=int, default=1)
+    parser.add_argument("--save_individual_segmentations", type=int, default=0)
+
+    args = parser.parse_args()
+
+    main_dicom.run(vars(args))
+
+if __name__ == "__main__":
+    main()
