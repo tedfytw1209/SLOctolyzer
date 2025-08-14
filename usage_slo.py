7bd790ec6d10f78ecd85079066de55cc45b20ddb
teddy <teddytw1209@gmail.com>
2025-08-13 22:38:50 -0400

    Update usage_slo.py

--- usage_slo.py
+++ usage_slo.py
@@ -5,7 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description="Run main_dicom with custom parameters.")
 
     parser.add_argument("--analysis_csv", default=r"/blue/ruogu.fang/tienyuchang/OCT_EDA/Paired_OCT_Data_ADCON_samples_part2.csv", help="Path to the analysis CSV file")
-    parser.add_argument("--output_directory", default=r"/blue/ruogu.fang/tienyuchang/SLO_Output", help="Directory to save the output")
+    parser.add_argument("--output_directory", default=r"/blue/ruogu.fang/tienyuchang/SLO_Output2", help="Directory to save the output")
     parser.add_argument("--robust_run", type=int, default=1)
     parser.add_argument("--save_individual_segmentations", type=int, default=0)
 
